46d8c1ee6fa9385e78ff8af9592e2a6ca1bee9b3
Preston Leach <prestonsleach@gmail.com>
2016-11-06 23:50:42 -0600

    Added logic for some tweet functions

--- src/main/java/com/cooksys/controller/TweetController.java
+++ src/main/java/com/cooksys/controller/TweetController.java
@@ -1,13 +1,54 @@
 package com.cooksys.controller;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.cooksys.entity.Credentials;
+import com.cooksys.entity.Tweet;
+import com.cooksys.repo.TweetRepo;
+
 
 @RestController
 @RequestMapping("tweets")
 public class TweetController {
-
-	public TweetController() {
+	
+	private TweetRepo tr;
+	Calendar calendar = Calendar.getInstance();
+	Timestamp currentTimestamp = new java.sql.Timestamp(calendar.getTime().getTime());
+	
+	public TweetController(TweetRepo tr) {
+		this.tr = tr;
+	}
+	
+	@GetMapping
+	public List<Tweet> getTweets() {
+		return tr.findByActiveTrue();
 		
+	}
+	
+	@PostMapping
+	public Tweet postTweet(@RequestBody Credentials creds, @RequestBody Tweet tweet) {
+		tweet.setActive(true);
+		tweet.setAuthor(creds.getUsername());
+		tweet.setPosted(currentTimestamp);
+		tr.saveAndFlush(tweet);
+		return tweet;
+	}
+	
+	@DeleteMapping("/{id}")
+	public Tweet deleteTweet(@PathVariable String id, @RequestBody Tweet tweet) {
+		tweet = tr.getTweetById(id);
+		tweet.setActive(false);
+		tr.saveAndFlush(tweet);
+		return tweet;
 	}
 }
--- src/main/java/com/cooksys/repo/TweetRepo.java
+++ src/main/java/com/cooksys/repo/TweetRepo.java
@@ -1,9 +1,14 @@
 package com.cooksys.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cooksys.entity.Tweet;
 
 public interface TweetRepo extends JpaRepository<Tweet, Long>{
+	
+	List<Tweet> findByActiveTrue();
 
+	Tweet getTweetById(String id);
 }
